20f5ca736a84cec644b9c4ca05fc8a306562e88b
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2022-12-25 22:59:17 +0100

    feat: add an user schema

---
+++ src/users/db/users.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ type: String, required: true })
+  username: string;
+
+  @Prop({ type: String, required: false })
+  email: string;
+
+  @Prop({ type: String, required: true })
+  dateOfRegistration: string;
+
+  @Prop({ type: String, required: true })
+  password: string;
+
+  @Prop({ type: String, required: false })
+  resetLink: string;
+
+  @Prop({ type: [String], required: true })
+  roles: string[];
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
